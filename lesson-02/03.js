3a1af3ff2edd960de09cfa7dcc77989dd025aa97
Romahahaha <romanchuk.matvey@mail.ru>
2026-03-19 19:20:40 +0300

    lesson 02-3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -8,22 +8,22 @@ const score = 70 // тестовое значение, можно изменят
 let grade
 // your code
 
-// if (score >=0 && <= 49)  {
-//     grade === 'F';
+if (score >= 0 && score <= 49)  {
+    grade === 'F';
     
-//  } else if (score >= 50 && <= 69) {
-//     grade === 'D';
-    
-
-//  } else if (score >= 70 && <= 79)  {
-//     grade === 'C';
+ } else if (score >= 50 && score <= 69) {
+    grade === 'D';
     
 
-// } else if (score >= 80 && <= 89)  {
-//       grade === 'B';
+ } else if (score >= 70 && score <= 79)  {
+    grade === 'C';
+    
 
-// } else if (score >= 90 && <= 100)
-//     {grade === 'A';} 
-//     else {
+} else if (score >= 80 && score <= 89)  {
+      grade === 'B';
+
+} else if (score >= 90 && score <= 100)
+    {grade === 'A';} 
+    else {
         
-//     };
+    };
