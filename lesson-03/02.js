303ed7f134f7a51b4dbf10168891800f5cf46d73
Romahahaha <romanchuk.matvey@mail.ru>
2026-09-01 19:13:24 +0300

    test1

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,10 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(firstNumber) {
+    if (firstNumber % 2 === 0 ) {
+        return true;
+    } else {
+        return false;
+    }
+}
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,20 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(firstNumber,secondNumber,thirdNumber) {
+    // if ((firstNumber > secondNumber && firstNumber > thirdNumber) && (secondNumber > thirdNumber)) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+let largest = firstNumber;
+
+     if (secondNumber > largest) {
+        largest = secondNumber;
+     } if (thirdNumber > largest) {
+        largest = thirdNumber;
+     }
+
+     return largest;
+
+}
